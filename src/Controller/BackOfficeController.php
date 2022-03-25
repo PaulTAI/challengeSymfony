bb7ec4380083c8a8f8c5c084cf8abe8b3aa1de2c
paulTAI <pta17856@open-groupe.com>
2022-03-25 01:50:50 +0100

    mise en place webpack sass

--- src/Controller/BackOfficeController.php
+++ src/Controller/BackOfficeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -17,6 +19,14 @@ class BackOfficeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('backOffice/layoutBackOffice.html.twig');
+        $file = new File('../public/files/toTry.pdf');
+        header('Content-type: application/pdf');
+        // header('Content-Disposition: inline; filename="' . $file . '"');
+        // header('Content-Tranfert-Encoding: binay');
+        // header('Accept-Ranges: bytes');
+        //$displayFile = readfile($file);
+        return $this->render('backOffice/layoutBackOffice.html.twig',[
+            // "filetry" => $displayFile
+        ]);
     }
 }
